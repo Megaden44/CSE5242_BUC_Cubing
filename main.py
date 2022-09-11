00c436c382acaa5012672e78755d8cdbd05f5b10
davecverano <davecverano@gmail.com>
2022-09-11 14:36:22 -0400

    create basic functions of sql table

--- main.py
+++ main.py
@@ -6,16 +6,64 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        return conn
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
 
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+    finally:
+        if c:
+            c.close()
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+    finally:
+        if c:
+            c.close()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    create_connection("...")
+    conn = create_connection(r"datacubing.db")
+
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                            id integer PRIMARY KEY,
+                                            A integer NOT NULL,
+                                            B integer NOT NULL,
+                                            C integer NOT NULL,
+                                            D integer NOT NULL,
+                                            E integer NOT NULL
+                                        ); """
+
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+
+
+    for i in range(10000):
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
