2eb8b9579f07cb079dfdb5a50be7fe8837bc1985
Megaden44 <54749205+Megaden44@users.noreply.github.com>
2022-09-24 11:55:56 -0400

    fixed buc algo

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from math import comb
 from sqlite3 import Error
 
 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by db_file
@@ -31,20 +40,7 @@ def create_table(conn, create_table_sql):
         print(e)
 
 
-def check_table(connection, table):
-    c = connection.cursor()
-    try:
-        query_check = f"""select COUNT(*) from {table}"""
-        c.execute(query_check)
-        return c.rowcount
-    except Error as e:
-        print(e)
-    finally:
-        if c:
-            c.close()
-
-
-def insert_table(conn, table, num_rows):
+def generate_table(conn, table, num_rows):
     """ insert into table specified number of rows
     :param num_rows: number of rows to insert
     :param conn: Connection object
@@ -114,9 +110,7 @@ def partition(conn, table_name, group_by_tuple):
     c = conn.cursor()
     try:
         if formatted_tuple:
-            query_check = \
-                f"""select COUNT(*), {formatted_tuple} from {table_name}
-                 group by {formatted_tuple}"""
+            query_check = f"""select COUNT(*), {formatted_tuple} from {table_name} group by {formatted_tuple}"""
         else:
             query_check = f"""select COUNT(*), A from {table_name}"""
         c.execute(query_check)
@@ -136,35 +130,70 @@ def partition(conn, table_name, group_by_tuple):
     finally:
         if c:
             c.close()
-    return 0
 
 
-def buc_cubing(conn, table_name, filter_level, dems, cur_level):
+def insert_into_table(conn, temp_table, parent_table, node, values):
+    """ insert into table specified number of rows
+      :param node: dimensions
+      :param conn: Connection object
+      :param temp_table: table to insert into
+      :param values: values to insert into table
+      :param parent_table: table to select from
+      :return:
+      """
+    c = conn.cursor()
+    try:
+        query_insert_into = f"""INSERT INTO {temp_table} SELECT * FROM {parent_table}"""
+        if len(node) > 0:
+            query_insert_into += " WHERE "
+            for index in range(len(node)):
+                query_insert_into += str(node[index]) + " = " + str(values[index + 1]) + " AND "
+            query_insert_into = query_insert_into[0:-5]
+        c.execute(query_insert_into)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+
+def buc_cubing(conn, parent_table, filter_level, buc_root, cur_level):
     """ cubes table_name using a BUC Iceberg approach
             :param conn: Connection object
-            :param table_name: name of table with partitions
+            :param parent_table: name of table with partitions
             :param filter_level: how deep the iceberg goes
-            :param dems: how deep the iceberg goes
-            :param cur_level: tracks the lattice layer for recursion
+            :param buc_root: root of processing tree
             :return:
     """
-    if cur_level == len(dems):
-        return
-    else:
-        num_dem = len(dems)
-        dimensions = comb(num_dem, cur_level)
-        # the nodes on a given level
-        lattice_nodes = list(combinations(dems, cur_level))
-        # for each permutation at a given lattice level
-        for d in range(dimensions):
-            # check of the aggregation sum for each group meets the filter level
-            # if it does stop, otherwise cube with only those values
-            result = partition(conn, table_name, lattice_nodes[d])
-            for row in result:
-                if row[0] > filter_level:
-                    buc_cubing(conn, table_name, filter_level, dems, cur_level + 1)
-                else:
-                    print("Number of groups is too sparse for node: " + str(lattice_nodes[d]) + ", group: " + str(row))
+    # for each permutation at a given lattice level
+    temp_table = "filtered_data_node_" + str(cur_level)
+    sql_create_temp_table = f""" CREATE TABLE IF NOT EXISTS {temp_table} (
+                                                        id integer PRIMARY KEY,
+                                                        A integer NOT NULL,
+                                                        B integer NOT NULL,
+                                                        C integer NOT NULL,
+                                                        D integer NOT NULL,
+                                                        E integer NOT NULL
+                                                    ); """
+    create_table(conn, sql_create_temp_table)
+    # check of the aggregation sum for each group meets the filter level
+    # if it does stop, otherwise cube with only those values
+    result = partition(conn, parent_table, buc_root.data)
+    for row in result:
+        if row[0] < filter_level:  # only add values to table that meet criteria
+            print("Number of groups is too sparse for node: " + str(buc_root.data) + ", group: " + str(row))
+        else:
+            insert_into_table(conn, temp_table, parent_table, buc_root.data, row)
+    for child_node in buc_root.children:
+        buc_cubing(conn, temp_table, filter_level, child_node, cur_level + 1)
+    drop_table(conn, temp_table)
+
+
+def buc_processing_tree(dems, root):
+    test = dems.copy()
+    while len(test) > 0:
+        d = test.pop(0)
+        child_node = Node(root.data + (d,))
+        root.add_child(child_node)
+        buc_processing_tree(test, child_node)
 
 
 def main():
@@ -173,7 +202,6 @@ def main():
     conn = create_connection(db_name)
     dems = ["A", "B", "C", "D", "E"]
     num_rows = 100000
-    output_arr = []
     sql_create_projects_table = f""" CREATE TABLE IF NOT EXISTS {table_name} (
                                             id integer PRIMARY KEY,
                                             A integer NOT NULL,
@@ -190,11 +218,15 @@ def main():
         # create projects table
         create_table(conn, sql_create_projects_table)
 
-        # insert_table(conn, table_name)
-        insert_table(conn, table_name, num_rows)
+        # add data to table
+        generate_table(conn, table_name, num_rows)
+
+        # create processing tree
+        buc_root = Node(())
+        buc_processing_tree(dems, buc_root)
 
         # run naive BUC cubing on data
-        buc_cubing(conn, table_name, 3, dems, 0)
+        buc_cubing(conn, table_name, 12500, buc_root, 0)
 
         # close connection
         conn.close()
